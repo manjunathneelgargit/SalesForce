d0ae4f3dd9d56f23761a52d362219fdd2ccd7ea8
Manjunath <Manjunath@TYSS-ELF-Manjunath>
2020-05-16 21:12:48 +0530

    First Commit

---
+++ WebElement/src/main/java/com/WebElement/AddWebElement/Capital.java
@@ -0,0 +1,14 @@
+package com.WebElement.AddWebElement;
+
+public class Capital {
+
+	public static void main(String[] args) 
+	{
+		String name = "tousif";
+		char fc = name.charAt(0);
+		System.out.println(fc);
+		System.out.println((char)(int)fc-32);
+
+	}
+
+}
